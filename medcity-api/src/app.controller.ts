6ec2656510ba8722edfbd81ae9ba4d359ba8ea12
Rubén Darío Carrillo López <rubendario199201@gmail.com>
2025-12-29 19:36:12 -0500

    feat(swagger): integrate Swagger for API documentation

--- medcity-api/src/app.controller.ts
+++ medcity-api/src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('App')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get Hello World message' })
   getHello(): string {
     return this.appService.getHello();
   }
