6159d727dee6444411c2a7e70286fe5d5e5cb30e
Rubén Darío Carrillo López <137409295+rubendario921@users.noreply.github.com>
2026-02-05 15:53:37 -0500

    Manejos de exceptios filtrando los errores

--- medcity-api/src/main.ts
+++ medcity-api/src/main.ts
@@ -1,19 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
+import { ResponseExceptionFilter } from './shared/filters/response-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Interceptors
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  //Filters
+  app.useGlobalFilters(new ResponseExceptionFilter());
+
   // Config Swagger
   const config = new DocumentBuilder()
     .setTitle('MedCity API')
     .setDescription('The MedCity API description')
     .setVersion('1.0')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  //Deploy
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ medcity-api/src/shared/filters/response-exception.filter.ts
@@ -0,0 +1,53 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { ApiResult } from '../contracts/api-result';
+
+@Catch()
+export class ResponseExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message: any = 'Error Desconocido';
+    let error: string = 'Error Desconocido';
+
+    if (exception instanceof HttpException) {
+      const exResponse: any = exception.getResponse();
+      status = exception.getStatus();
+      message = 
+        typeof exResponse === 'string'
+          ? exResponse
+          : exResponse?.message ?? exResponse;
+      error =
+        exResponse && typeof exResponse === 'object'
+          ? exResponse.error ?? exception.name
+          : exception.name;
+
+    } else if (exception instanceof Error) {
+      message = exception.message || message;
+      error = exception.name || error;
+    }
+
+    const normalizedMessage = Array.isArray(message)
+      ? message.join(', ')
+      : message;
+
+    const body: ApiResult<null> = {
+      timestamp: new Date().toISOString(),
+      statusCode: status,
+      message: normalizedMessage,
+      error,
+      data: null,
+      path: request.url,
+    };
+
+    response.status(status).json(body);
+  }
+}
